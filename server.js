3349d9c4c7c9136991381c54a4c6c4155025d3d0
SananKarim <65340880+SananKarim@users.noreply.github.com>
2024-02-28 12:53:35 +0500

    rm lib

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 const express = require("express");
-const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bot-db",
-});
-
 // Check if the database connection is successful
 app.get("/", (req, res) => {
-  return res.json("testing");
-});
-
-app.get("/user", (req, res) => {
-  const sql = "SELECT * FROM WORKER";
-  db.query(sql, (error, data) => {
-    if (error) {
-      console.error("Error querying database:", error);
-      return res.json(error);
-    }
-    return res.json(data);
-  });
+  return res.json("i am alive");
 });
 
 // Listen on port 80 for incoming HTTP requests
-app.listen(80, () => {
-  console.log("Server is running on port 80");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
